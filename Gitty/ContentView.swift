c741b15260560d1f0bb840463223251b4aa08e4f
James <james@eli.cx>
2026-01-02 19:08:15 +0000

    Initial Commit

---
+++ Gitty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gitty
+//
+//  Created by James on 02/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gitty/GittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittyApp.swift
+//  Gitty
+//
+//  Created by James on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct GittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
